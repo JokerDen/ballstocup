b1fdab3b0de00df2080b746bdfc5508df3618161
Denis Kazantsev <jokerdenkazantsev@gmail.com>
2022-07-28 21:51:29 +0300

    Add custom ball params

--- Assets/Scripts/gameplay/Ball.cs
+++ Assets/Scripts/gameplay/Ball.cs
@@ -7,6 +7,22 @@ namespace gameplay
      */
     public class Ball : MonoBehaviour
     {
-        
+        [SerializeField] bool overwritePhysicMaterial;
+        [SerializeField] float bounce;
+        [SerializeField] float friction;
+
+        private void Start()
+        {
+            if (!overwritePhysicMaterial) return;
+            
+            var cols = GetComponentsInChildren<Collider>();
+            var mat = new PhysicMaterial();
+            mat.bounciness = bounce;
+            mat.dynamicFriction = friction;
+            mat.staticFriction = friction;
+            
+            foreach (var col in cols)
+                col.material = mat;
+        }
     }
 }
